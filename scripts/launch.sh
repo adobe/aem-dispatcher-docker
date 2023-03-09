0527e29b172bdf90ef94a92d2b30448aafb05ed2
marcotbp <39282526+marcotbp@users.noreply.github.com>
2023-03-09 13:18:57 -0300

    Clear run files to support container restart via docker compose (#7)

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -17,4 +17,8 @@
 /usr/bin/cat /NOTICE
 
 /usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg
+
+rm -f /run/httpd/authdigest_shm.*
+rm -f /run/httpd/cgisock.*
+rm -f /run/httpd/httpd.pid
 /usr/sbin/httpd -D FOREGROUND
