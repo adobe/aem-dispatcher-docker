42853bc1d423ad2db875edac8bf6e6cc6a430b28
Robert Botha <botha@adobe.com>
2023-05-24 19:22:37 +0100

    Add ENV_TYPE and RUNMODE variables to match latest AMS blocks

--- scripts/env.sh
+++ scripts/env.sh
@@ -41,3 +41,6 @@ CRX_FILTER=deny
 ## WARNING: Set this to "allowed" on local dev environments that don't have fixed IPs
 ## Set to deny or comment out on prod environments
 DISPATCHER_FLUSH_FROM_ANYWHERE=allow
+
+ENV_TYPE=dev
+RUNMODE=sites
